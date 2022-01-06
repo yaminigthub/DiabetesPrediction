85f97b8e55ea4bcdfa277e79ebc4984a0a328404
yaminigthub <yaminits08@gmail.com>
2022-01-06 19:47:57 +0530

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 
 # Load the Random Forest CLassifier model
-filename = 'diabetes-prediction-rfc-model.pkl'
+filename = 'diabetes-prediction-lgbm-model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
